696c85ea68b25e7025cb6677d51e2e776d20f85f
Subhanu Sankar Roy <71728589+SubhanuSRoy@users.noreply.github.com>
2024-04-19 20:01:45 +0530

    ft. added user schema

---
+++ models/user.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose');
+
+// make schema for the user
+const userSchema = new mongoose.Schema({
+  email: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+  location: String,
+  age: Number,
+  workDetails: String,
+  isVerified: { type: Boolean, default: false },
+});
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
